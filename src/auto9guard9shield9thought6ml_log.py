b739fe27fdcb4cf9f7b0b8300272ac15f95df8de
brookbrook42 <silentbrook42@gmail.com>
2025-04-16 12:35:04 +0900

    Update development log at 2025-04-16 12:35:03

--- src/auto9guard9shield9thought6ml_log.py
+++ src/auto9guard9shield9thought6ml_log.py
@@ -203,3 +203,15 @@ def hash_password(password):
 
 def verify_password(password, hashed):
     return hash_password(password) == hashed
+
+# Update at 2025-04-16 12:35:03
+# Refactored the code
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
