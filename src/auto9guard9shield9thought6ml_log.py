e1eb8bab7ea4fa7c2b4ed9cd120ee99344e7c9e9
brookbrook42 <silentbrook42@gmail.com>
2025-05-12 13:14:11 +0900

    Update development log at 2025-05-12 13:14:10

--- src/auto9guard9shield9thought6ml_log.py
+++ src/auto9guard9shield9thought6ml_log.py
@@ -626,3 +626,19 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+# Update at 2025-05-12 13:14:10
+# Refactored the code
+# Added unit tests
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
