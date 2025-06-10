5e6cf307416d1efe0546861bebe466882d3ce3af
brookbrook42 <silentbrook42@gmail.com>
2025-06-10 21:41:11 +0900

    Update development log at 2025-06-10 21:41:09

--- src/auto9guard9shield9thought6ml_log.py
+++ src/auto9guard9shield9thought6ml_log.py
@@ -1104,3 +1104,18 @@ def save_to_json(data, filename):
 def load_from_json(filename):
     with open(filename, 'r') as f:
         return json.load(f)
+
+# Update at 2025-06-10 21:41:09
+# Refactored the code
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
