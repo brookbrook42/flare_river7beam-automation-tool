ff59e333a836775d5e5f5c475c8a91d7d8f68eba
brookbrook42 <silentbrook42@gmail.com>
2025-11-23 04:21:06 +0900

    Update development log at 2025-11-23 04:21:05

--- src/auto9guard9shield9thought6ml_log.py
+++ src/auto9guard9shield9thought6ml_log.py
@@ -1603,3 +1603,14 @@ def save_to_json(data, filename):
 def load_from_json(filename):
     with open(filename, 'r') as f:
         return json.load(f)
+
+# Update at 2025-11-23 04:21:05
+# Fixed some bugs
+import hashlib
+
+def hash_password(password):
+    salt = "random_salt"
+    return hashlib.sha256((password + salt).encode()).hexdigest()
+
+def verify_password(password, hashed):
+    return hash_password(password) == hashed
